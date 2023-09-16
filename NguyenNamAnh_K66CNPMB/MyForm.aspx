3b5a22ad37664f31f6e625f022a27bdf51a9759c
GV07 <GV07@DESKTOP-P0GENIN>
2023-09-16 15:38:23 +0700

    Add project files.

---
+++ NguyenNamAnh_K66CNPMB/MyForm.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MyForm.aspx.cs" Inherits="NguyenNamAnh_K66CNPMB.MyForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ NguyenNamAnh_K66CNPMB/MyForm.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NguyenNamAnh_K66CNPMB.ServiceReference1;
+
+namespace NguyenNamAnh_K66CNPMB
+{
+    public partial class MyForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WebServiceSoapClient myService = new WebServiceSoapClient();
+
+            Response.Write("<h3> Giờ hệ thống hiện tại là: " + myService.LayGioHeThong() + "</h3>");
+            Response.Write("<h3> Số thứ nhất: 500. Số thứ hai là: 400. Vậy tổng là: " + myService.TinhTong(500,400) + "</h3>");
+
+        }
+    }
+}
